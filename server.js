748a7db03b9c5f44c1ff26a917e6460e95fa1d95
Daniel Hoover <daniel.hoover@outlook.com>
2022-01-11 19:49:38 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+// Setup empty JS object to act as endpoint for all routes
+let projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+const port = 3000;
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+const corsOptions ={
+    origin:'*', 
+ }
+app.use(cors(corsOptions));
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+app.listen(port, () => console.log('my server is running on port ' + port));
+
+// Setup Routes
+const returnData = (request, response) => {
+    response.send(projectData);
+    console.log('get was called')
+}
+app.get('/retrieve', returnData)
+
+
+const addData = (request, response) => {
+    projectData = {
+        temperature: request.body.temperature,
+        date: request.body.date,
+        userZip: request.body.userZip,
+        userFeelings: request.body.userFeelings,
+    };
+    console.log('post was called, request.body is:');
+    console.log(request.body);
+    response.send(projectData);
+}
+app.post('/add', addData)
---
+++ website/app.js
@@ -0,0 +1,73 @@
+/* Global Variables */
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=4b6f955094d76823d514ab6bf64a2680&units=imperial';
+const generate = document.querySelector('#generate');
+const postPath = 'http://localhost:3000/add'
+
+// api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Fetch weather by zip
+const checkWeather = async (zip) => {
+    const endpoint = baseURL + zip + apiKey;
+    const data = await fetch(endpoint).then(res => res.json());
+    console.log(data);
+    return data;
+}
+
+// Save data
+const postWeather = async (path, data) => {
+    const response = await fetch(path, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+    try {
+        const updatedData = await response.json();
+        console.log('ran postWeather, response below:');
+        console.log(updatedData)
+        return updatedData; 
+    }catch(error) {
+        console.log('error', error);
+    }
+
+}
+
+// retrieve latest data
+const getData = async (res) => {
+    console.log('running get data with passed response:', res)
+    const data = await fetch('http://localhost:3000/retrieve').then(res => res.json());
+    console.log('ran getData, should be data below');
+    console.log(data);
+    updateUI(data);
+}
+
+// update with latest data
+const updateUI = (data) => {
+    document.querySelector('#date').textContent = data.date;
+    document.querySelector('#temp').textContent = data.temperature;
+    document.querySelector('#content').textContent = data.userFeelings;
+}
+
+
+// listen for submit then call apis
+const handleGenerate = (event) => {
+    const userZip = document.querySelector('#zip').value;
+    const userFeelings = document.querySelector('#feelings').value;
+    event.preventDefault();
+    checkWeather(userZip)
+        .then(data => postWeather(postPath, {
+           temperature: data.main.temp,
+           date: newDate,
+           userZip,
+           userFeelings, 
+        }))
+        .then(res => getData(res));
+}
+generate.addEventListener('click', handleGenerate);
