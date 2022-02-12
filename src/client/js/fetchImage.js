2dd62864ed3534b3c853eced8e6cfc71c4d19817
systemshaper <Daniel.Hoover@outlook.com>
2022-02-12 17:24:04 -0600

    add location image fetch function

---
+++ src/client/js/fetchImage.js
@@ -0,0 +1,14 @@
+const pixabayKey = '25676820-bcd461f51d45cd7adb043ce89'
+
+const fetchImage = async (searchInput) => {
+    const url = `https://pixabay.com/api/?key=${pixabayKey}&q=${searchInput}&image_type=photo&category=travel`
+    const imageResults = await fetch(url)
+        .then(res => res.json())
+    
+    const imageUrl = imageResults['hits'][0]['webformatURL']
+    
+    console.log('image url: ', imageUrl)
+    return imageUrl
+}
+
+export { fetchImage }
